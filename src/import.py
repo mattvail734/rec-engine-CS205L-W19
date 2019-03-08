682ddfbc5fc7c7ffced8c322b49661cc1f2d1397
Matt <mattrvail@gmail.com>
2019-03-08 09:55:37 -0800

    add references

--- src/import.py
+++ src/import.py
@@ -7,4 +7,12 @@ from surprise import Reader, Dataset, SVD, evaluate
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+# download the netflix prize datatset from
+# https://www.kaggle.com/netflix-inc/netflix-prize-data/downloads/netflix-prize-data.zip/1
+# unzip and place the entire 'netflix-prize-data' folder in repo (not tracked by git) before running
+
 sns.set_style('darkgrid')
+
+df1 = pd.read_csv('../netflix-prize-data/combined_data_1.txt')
+print(df1.head())
